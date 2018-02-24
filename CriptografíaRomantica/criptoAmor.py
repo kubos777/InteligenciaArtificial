9ee8a7121fa2344a5bcc40c2809e3fcb95184f05
kubos777 <chd.jl@hotmail.com>
2018-02-23 21:03:32 -0600

    Subo función que desencripta

--- CriptografíaRomantica/criptoAmor.py
+++ CriptografíaRomantica/criptoAmor.py
@@ -1,5 +1,6 @@
 #-*- coding:utf-8-*-	
 import os
+import copy
 def asciiart():
 	"""
 	Función que me imprime un mensaje con ASCII art
@@ -52,35 +53,60 @@ def encriptar(palabraClave,texto):
 	#Íteramos por todo el texto, asígnando la letra correspondiente
 	#de acuerdo al modulo de cada elemento de la lista palabraClave
 	for j in range(len(texto)):
-		print(palabraClave[j%len(palabraClave)])
 		dicc[palabraClave[j%len(palabraClave)]]= dicc[palabraClave[j%len(palabraClave)]]+texto[j]
 	
+	aux_dicc=dicc
 	#print(dicc)
-	
 	#print(dicc.keys())
-
 	diccPalabraClave=list(dicc.keys())
 	#print(diccPalabraClave)
 	#Ordenamos la lista
-
 	diccPalabraClave.sort()
-
 	for i in diccPalabraClave:
 		textoEncriptado=textoEncriptado+dicc[i]
-
-	print("El texto encriptado es: ",textoEncriptado)
-
-	palEncr=open("palabrasEncriptadas","a+")
-	palEncr.write("\n"+textoEncriptado)
-
+	#print("El texto encriptado es: ",textoEncriptado)
+	palEncr=open("palabrasEncriptadas","w+")
+	palEncr.write(textoEncriptado)
 	funciona=palEncr.read()
-	print(funciona)
-def desencriptar(palabraClave,texto):
-	pass
+	#print(funciona)
+	palEncr.close()
+	return textoEncriptado
+def desencriptar(palabraClave):
+	"""
+	Funcion que desencripta el mensaje leído de un archivo
+	Recíbe como argumento la palabraClave
+	"""
+	palEncr=open("palabrasEncriptadas","r")
+	textoEncriptado=palEncr.read()
+	#textoEncriptado=list(textoEncriptado)
+	copiaPalabraClave=palabraClave.copy()
+	copiaPalabraClave=''.join(copiaPalabraClave)
+	palabraClave.sort()
+	dicc={}	
+	for i in palabraClave:
+		dicc[i]=""
+
+	div=int(len(textoEncriptado)/len(palabraClave))	
+	inicio=0
+	fin=div
+	for i in range(len(palabraClave)):	
+		print("Inicio: ",inicio,"Fin: ",div)
+		dicc[palabraClave[i]]=textoEncriptado[inicio:fin]
+		#rest.append(textoEncriptado[inicio:div])
+		inicio=inicio+div
+		fin=fin+div
+
+	print(dicc)
 	
+	desencriptado=""
+	print(div)
 
-
-
+	for i in range(div):
+		for j in copiaPalabraClave:
+			desencriptado=desencriptado+dicc[j][i]
+	#print("Desencriptado: ",desencriptado)
+	return desencriptado
+	
 def main(opcion):
 	"""
 	Función principal, muestra el menu y llama a las funciones correspondientes
@@ -90,16 +116,23 @@ def main(opcion):
 	if opcion=='1':
 		os.system('clear')
 		palabraClave=input("\nIngresa tu palabra clave:   ")
-		texto="La criptografía es romántica"
-		#texto=input("\nIngresa el texto:")
-		encriptar(palabraClave,texto)
-	
+		palabraClave=palabraClave.lower()
+		print(palabraClave)
+		palabraClave=list(palabraClave)
+		#texto="La criptografía es romántica"
+		texto=input("\nIngresa el texto:")
+		textoEncriptado=encriptar(palabraClave,texto)
+		print("La palabra encriptada es: ",textoEncriptado)
 	elif opcion=='2':
 		os.system('clear')
 		palabraClave=input("\nIngresa tu palabra clave:   ")
+		palabraClave=palabraClave.lower()
+		palabraClave=list(palabraClave)
 		texto="La criptografía es romántica"
 		#texto=input("\nIngresa el texto:")
-		desencriptar(palabraClave,texto,textoEncriptado)
+		
+		desencriptado=desencriptar(palabraClave)
+		print("Mensaje desencriptado: ",desencriptado)
 	elif opcion=='3':
 		print("Vuelve pronto!")
 		exit()
@@ -111,5 +144,4 @@ if __name__=='__main__':
 	while True:
 		asciiart()
 		opcion=input("\nElige una opción para continuar:\n\t1.-Encriptar\n\t2.-Desencriptar\n\t3.-Salir\n")
-		
 		main(opcion)
